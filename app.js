3111d0f0ee4e9b5767506b091ba4a68db268622c
Otavio Bruno Leite Barbosa <1831408@ifrn.local>
2016-12-01 20:49:09 -0300

    Modificacoes

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ var app = angular.module('ngApp', []);
 
 app.controller('ContatosCtrl', function($scope) {
 	$scope.contatos = [
-		{nome: 'Otavio', telefone: '9999-8888', email: 'otavio@email.com', salario: 500, data: Date.now()},
-		{nome: 'Raquel', telefone: '1234-5678', email: 'raquel@email.com', salario: 12000.34, data: Date.now()},
-		{nome: 'Jailson', telefone: '1111-2222', email: 'jailson@email.com', salario: 3000, data: Date.now()},
+		{nome: 'Otavio', telefone: '9999-8888', email: 'otavio@email.com'},
+		{nome: 'Raquel', telefone: '1234-5678', email: 'raquel@email.com'},
+		{nome: 'Jailson', telefone: '1111-2222', email: 'jailson@email.com'},
 	];
+
+	$scope.addContato = function(contato) {
+		$scope.contatos.push(angular.copy(contato));
+		delete $scope.contato;
+	}
+
+	$scope.delContato = function(contatos) {
+		$scope.contatos = contatos.filter(function(c) {
+			if(!c.selecionado) {
+				return c;
+			}
+		});
+	}
 });
